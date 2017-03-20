abd67e140a7c6058d98153fca86c8df0791a5bc7
T Puff <T Puff>
2017-03-20 01:06:33 -0500

    Created a button function

--- assets/app.js
+++ assets/app.js
@@ -17,15 +17,18 @@ $(document).ready( function() {
 		'Taylor Swift'
 	];
 
-	var gifURL;
-	var gifStillURL;
+	function createCelebBtns() {
+		$('.buttonsDiv').empty();
 
-	for (var i=0; i < topics.length; i++){
-		var newBtn = $('<button class="btn btn-info btn-sm celeb-btn">' + topics[i] + '</button>');
-		var celebName = topics[i].toLowerCase().replace(/\s/g, '');
-		newBtn.attr('data-person', celebName);
-		$('.buttonsDiv').append(newBtn);
+		for (var i=0; i < topics.length; i++){
+			var newBtn = $('<button class="btn btn-info btn-sm celeb-btn">' + topics[i] + '</button>');
+			var celebName = topics[i].toLowerCase().replace(/\s/g, '');
+			newBtn.attr('data-person', celebName);
+			$('.buttonsDiv').append(newBtn);
+		}
 	}
+
+	createCelebBtns();
 
 	$('body').on('click', '.celeb-btn', function(e) {
 		e.preventDefault();
@@ -53,9 +56,9 @@ $(document).ready( function() {
 	            var h4 = $('<h4>').text('Rating: ' + rating);
 
 	            var personGif = $('<img>');
-	            personGif.attr('src', results[i].images.fixed_height.url);
+	            personGif.attr('src', results[i].images.fixed_height_still.url);
 	            personGif.attr('class', 'gif');
-	            personGif.attr('data-state', 'animate');
+	            personGif.attr('data-state', 'still');
 	            personGif.attr('data-animate', results[i].images.fixed_height.url);
 	            personGif.attr('data-still', results[i].images.fixed_height_still.url);
 
@@ -69,29 +72,22 @@ $(document).ready( function() {
 
 	$(document).on('click', '.gif', function() {
 		var state = $(this).attr('data-state');
-      	console.log(state);
 
       	var dataAnimateURL = $(this).attr('data-animate');
       	var dataStillURL = $(this).attr('data-still');
 
-	    if (state === 'animate') {
-	    	$(this).attr('src', dataStillURL);
-	        $(this).attr('data-state', 'still');
+	    if (state === 'still') {
+	    	$(this).attr('src', dataAnimateURL);
+	        $(this).attr('data-state', 'animate');
 	    }
 	    else {
-	        $(this).attr('src', dataAnimateURL);
-	        $(this).attr('data-state', 'animate');
+	        $(this).attr('src', dataStillURL);
+	        $(this).attr('data-state', 'still');
 	    }
 	})
 
 	$('.submit-btn').on('click', function () {
-		var newCelebName = $('input').val().toLowerCase().replace(/\s/g, '');
-		console.log(newCelebName);
-
-		var newBtn = $('<button class="btn btn-info btn-sm celeb-btn">' + 
-			$('input').val() + '</button>');
-
-		newBtn.attr('data-person', newCelebName);
-		$('.buttonsDiv').append(newBtn);
+		topics.push($('input').val());
+		createCelebBtns();
 	});
 });
